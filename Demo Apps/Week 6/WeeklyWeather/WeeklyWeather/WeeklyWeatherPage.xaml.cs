a8a95ee3f6636a55a4f86661f38be74f3bc54f3e
brett@sheleski.com <brett@sheleski.com>
2018-02-26 13:21:30 -0600

    Initial add for WeeklyWeather demo app

---
+++ Demo Apps/Week 6/WeeklyWeather/WeeklyWeather/WeeklyWeatherPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace WeeklyWeather
+{
+    public partial class WeeklyWeatherPage : ContentPage
+    {
+        public WeeklyWeatherPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
