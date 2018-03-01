0d10da5a9cdc6b5ba07794897cf808e75ad9de9d
brett@sheleski.com <brett@sheleski.com>
2018-02-28 18:37:31 -0600

    Initial Commit of demo app

---
+++ Demo Apps/Week 6/WeeklyWeather/WeeklyWeather/WeatherSummaryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WeeklyWeather
+{
+    public partial class WeatherSummaryPage : ContentPage
+    {
+        public WeatherSummaryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
