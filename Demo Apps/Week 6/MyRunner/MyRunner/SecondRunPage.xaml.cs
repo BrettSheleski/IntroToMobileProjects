16653fb6e6d7ea060d9ee130e71bf78f2d777661
brett@sheleski.com <brett@sheleski.com>
2018-03-01 08:12:25 -0600

    Updates.  Just 1 bug remaining.

--- Demo Apps/Week 6/MyRunner/MyRunner/SecondRunPage.xaml.cs
+++ Demo Apps/Week 6/MyRunner/MyRunner/SecondRunPage.xaml.cs
@@ -11,5 +11,20 @@ namespace MyRunner
         {
             InitializeComponent();
         }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            double miles, minutes, seconds;
+
+            double.TryParse(MilesEntry.Text, out miles);
+            double.TryParse(MinutesEntry.Text, out minutes);
+            double.TryParse(SecondsEntry.Text, out seconds);
+
+            TimeSpan time = TimeSpan.FromMinutes(minutes).Add(TimeSpan.FromSeconds(seconds));
+
+            App.RunCalculator.Run2Time = time;
+            App.RunCalculator.Run2Distance = miles;
+        }
     }
 }
--- Demo Apps/Week 6/MyRunner/MyRunner/ThirdRunPage.xaml.cs
+++ Demo Apps/Week 6/MyRunner/MyRunner/ThirdRunPage.xaml.cs
@@ -11,5 +11,21 @@ namespace MyRunner
         {
             InitializeComponent();
         }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            double miles, minutes, seconds;
+
+            double.TryParse(MilesEntry.Text, out miles);
+            double.TryParse(MinutesEntry.Text, out minutes);
+            double.TryParse(SecondsEntry.Text, out seconds);
+
+            TimeSpan time = TimeSpan.FromMinutes(minutes).Add(TimeSpan.FromSeconds(seconds));
+
+            App.RunCalculator.Run3Time = time;
+            App.RunCalculator.Run3Distance = miles;
+        }
     }
 }
