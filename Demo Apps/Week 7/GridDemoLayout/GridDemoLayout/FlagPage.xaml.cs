4349ff260a82f0d62116eb2c8e03030853959ef8
brett@sheleski.com <brett@sheleski.com>
2018-03-07 10:29:35 -0600

    Added Week7 Demo app

---
+++ Demo Apps/Week 7/GridDemoLayout/GridDemoLayout/FlagPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GridDemoLayout
+{
+    public partial class FlagPage : ContentPage
+    {
+        public FlagPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Demo Apps/Week 7/GridDemoLayout/GridDemoLayout/FlagPageCS.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace GridDemoLayout
+{
+    public class FlagPageCS : ContentPage
+    {
+        public FlagPageCS()
+        {
+            Grid theGrid = new Grid();
+
+            theGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(100) });
+            theGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+
+            BoxView blueBox = new BoxView
+            {
+                Color = Color.Blue
+            };
+
+            const int blueBoxRowSpan = 7;
+
+            Grid.SetRow(blueBox, 0);
+            Grid.SetColumn(blueBox, 0);
+            Grid.SetRowSpan(blueBox, blueBoxRowSpan);
+
+            theGrid.Children.Add(blueBox);
+
+            BoxView stripeBox;
+            for (int i = 0; i < 13; ++i){
+                theGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+
+                stripeBox = new BoxView
+                {
+                    Color = i % 2 == 0 ? Color.Red : Color.White
+                };
+
+                Grid.SetRow(stripeBox, i);
+
+                if (i < blueBoxRowSpan){
+                    Grid.SetColumn(stripeBox, 1);
+                    Grid.SetColumnSpan(stripeBox, 1);
+                }
+                else{
+                    Grid.SetColumn(stripeBox, 0);
+                    Grid.SetColumnSpan(stripeBox, 2);
+                }
+
+
+                theGrid.Children.Add(stripeBox);
+            }
+
+
+            Content = theGrid;
+        }
+    }
+}
+
