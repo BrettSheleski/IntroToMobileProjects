cac1c9d1b4d32b48690af3f3a4c11e608e1557be
brett@sheleski.com <brett@sheleski.com>
2018-03-08 06:16:42 -0600

    h

---
+++ Demo Apps/Week 7/GridDemoLayout/GridDemoLayout/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.Forms;
+
+namespace GridDemoLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Page firstPage = new GridDemoLayoutPage();
+
+            if (Device.RuntimePlatform == Device.iOS){
+
+                NavigationPage navPage = new NavigationPage(firstPage);
+
+                MainPage = navPage;
+            }
+            else{
+                MainPage = firstPage;    
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Demo Apps/Week 7/GridDemoLayout/GridDemoLayout/GridDemoLayoutPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GridDemoLayout
+{
+    public partial class GridDemoLayoutPage : ContentPage
+    {
+        public GridDemoLayoutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void HandleShowFlagXaml_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FlagPage());
+        }
+
+        async void HandleShowFlagCS_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FlagPageCS());
+        }
+
+    }
+}
