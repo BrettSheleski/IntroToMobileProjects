0dd73de3280d5dad12f2d02d2d3a7e7e42eabe07
Brett Sheleski <brett@sheleski.com>
2018-03-15 14:41:58 -0500

    Added AsyncAwait supplementary project

---
+++ Demo Apps/Supplementary/AsyncAwait/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.TheProgressBar = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start the Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Location = new System.Drawing.Point(124, 32);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(0, 13);
+            this.MessageLabel.TabIndex = 1;
+            this.MessageLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start The Process WIth Async";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(48, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(294, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Show Progress";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // TheProgressBar
+            // 
+            this.TheProgressBar.Location = new System.Drawing.Point(127, 301);
+            this.TheProgressBar.Name = "TheProgressBar";
+            this.TheProgressBar.Size = new System.Drawing.Size(100, 23);
+            this.TheProgressBar.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(395, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 368);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.TheProgressBar);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.MessageLabel);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label MessageLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar TheProgressBar;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Demo Apps/Supplementary/AsyncAwait/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isFirstDownloadCompleted = false;
+        bool isSecondDownloadCompleted = false;
+        bool isThirdDownloadCompleted = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "Downloading Stuff, please wait.";
+
+            isFirstDownloadCompleted = false;
+            isSecondDownloadCompleted = false;
+            isThirdDownloadCompleted = false;
+
+            BackgroundWorker backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+            backgroundWorker.RunWorkerAsync();
+
+            BackgroundWorker bw2 = new BackgroundWorker();
+            bw2.DoWork += Bw2_DoWork;
+            bw2.RunWorkerCompleted += Bw2_RunWorkerCompleted;
+            bw2.RunWorkerAsync();
+
+            BackgroundWorker bw3 = new BackgroundWorker();
+            bw3.DoWork += Bw3_DoWork;
+            bw3.RunWorkerCompleted += Bw3_RunWorkerCompleted;
+            bw3.RunWorkerAsync();
+
+        }
+
+        private void Bw3_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ShowCompletedMessage();
+        }
+
+        private void Bw3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DownloadAThirdLargeFile();
+            isThirdDownloadCompleted = true;
+        }
+
+        private void Bw2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ShowCompletedMessage();
+        }
+
+        private void ShowCompletedMessage()
+        {
+            if (isFirstDownloadCompleted && isSecondDownloadCompleted && isThirdDownloadCompleted)
+            {
+                MessageLabel.Text = "Done Downloading, yahoo!";
+            }
+        }
+
+        private void Bw2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DownloadAnotherLargeFile();
+            isSecondDownloadCompleted = true;
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ShowCompletedMessage();
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DownloadLargeFile();
+
+            isFirstDownloadCompleted = true;
+        }
+
+        private Task DownloadAThirdLargeFileAsync()
+        {
+            return Task.Factory.StartNew(DownloadAThirdLargeFile);
+        }
+
+        private void DownloadAThirdLargeFile()
+        {
+            // not actually downloading large file
+            // simulating downloading large file by 
+            // locking up the current thread
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+        }
+
+        private Task DownloadAnotherLargeFileAsyc()
+        {
+            return Task.Factory.StartNew(DownloadAnotherLargeFile);
+        }
+
+        private void DownloadAnotherLargeFile()
+        {
+            // not actually downloading large file
+            // simulating downloading large file by 
+            // locking up the current thread
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
+        private Task DownloadLargeFileAsync()
+        {
+            return Task.Factory.StartNew(DownloadLargeFile);
+        }
+
+        private void DownloadLargeFile()
+        {
+            // not actually downloading large file
+            // simulating downloading large file by 
+            // locking up the current thread
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "Downloading Stuff, please wait.";
+            
+            await Task.WhenAll(
+                DownloadLargeFileAsync(), 
+                DownloadAnotherLargeFileAsyc(), 
+                DownloadAThirdLargeFileAsync()
+            );
+  
+            MessageLabel.Text = "Done.  yay!";
+        }
+
+
+        
+
+
+        public async Task DoSomethingAsync(string arg1, int count, CancellationToken cancellationToken, IProgress<double> progress)
+        {
+            for(int i = 0; i < count; ++i)
+            {
+                await Task.Delay(10);
+                
+                if (cancellationToken.IsCancellationRequested)
+                    break;
+                else
+                    progress.Report(i / (double)count);
+            }
+        }
+
+        CancellationTokenSource _cancellationTokenSource;
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            TheProgressBar.Minimum = 0;
+            TheProgressBar.Maximum = 100;
+            Progress<double> progress = new Progress<double>(d => {
+                TheProgressBar.Value = (int)(d * 100);
+            });
+
+
+            _cancellationTokenSource = new CancellationTokenSource();
+            await DoSomethingAsync("", 2000, _cancellationTokenSource.Token, progress);
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
+
+            TheProgressBar.Value = 0;
+        }
+    }
+}
