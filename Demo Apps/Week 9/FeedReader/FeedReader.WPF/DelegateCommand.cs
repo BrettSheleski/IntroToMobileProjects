60e6cfb14f87699ba5896c8b5f29d081ff76e3c8
Brett Sheleski <brett@sheleski.com>
2018-03-22 08:09:48 -0500

    Added WPF Project

---
+++ Demo Apps/Week 9/FeedReader/FeedReader.WPF/DelegateCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FeedReader.WPF
+{
+    
+    class DelegateCommand<T> : ICommand
+    {
+        public DelegateCommand(Action<T> action)
+        {
+            this.Action = action;
+        }
+
+        public Action<T> Action { get; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Action((T)parameter);
+        }
+
+        protected void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Demo Apps/Week 9/FeedReader/FeedReader.WPF/FeedListViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FeedReader.Core.Models;
+
+namespace FeedReader.WPF
+{
+    public class FeedListViewModel : Core.Model
+    {
+        public FeedListViewModel()
+        {
+            this.Model = new Core.Models.FeedListMvvmModel();
+            this.OpenArticleCommand = new DelegateCommand<string>(OpenArticle);
+        }
+
+        public FeedListMvvmModel Model { get; }
+        public ICommand OpenArticleCommand { get; }
+
+        private void OpenArticle(string url)
+        {
+            Process.Start(new ProcessStartInfo(url));   
+        }
+
+    }
+}
