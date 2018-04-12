8ebbc127d2b49b62b5bd57133cfe102a1b599481
Brett Sheleski <brett@sheleski.com>
2018-04-12 13:45:21 -0500

    Changes.

--- Demo Apps/Supplementary/EmbeddedResourcesExample/EmbeddedResourcesExample/EmbeddedResourcesExample/MainPage.xaml.cs
+++ Demo Apps/Supplementary/EmbeddedResourcesExample/EmbeddedResourcesExample/EmbeddedResourcesExample/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -30,7 +31,7 @@ namespace EmbeddedResourcesExample
 
 
             using (var resourceStream = assembly.GetManifestResourceStream(myDataJsonResourceName))
-                using (var reader = new System.IO.StreamReader(resourceStream))
+            using (var reader = new System.IO.StreamReader(resourceStream))
             {
 
                 theJson = reader.ReadToEnd();
@@ -42,7 +43,111 @@ namespace EmbeddedResourcesExample
 
 
 
+            
+
+
+
+
+
             MyList.ItemsSource = people;
         }
+
+
+
+
+
+        void Foo()
+        {
+
+
+
+            int sum = Add(2, 3);
+
+
+            Func<int, int, int> SomeMethodThatTakesInTwoIntsAndReturnAnInt = Add;
+
+            sum = SomeMethodThatTakesInTwoIntsAndReturnAnInt(10, 20);
+
+            Func<int, int, double> someMethod = Divide;
+
+
+            someMethod = delegate (int x, int y)
+            {
+                return x / (double)y;
+            };
+
+
+            someMethod = (q, p) => q / (double)p;
+
+            someMethod = (x1, x2) =>
+            {
+                Console.WriteLine($"Dividing {x1} by {x2}");
+
+                return x1 / (double)x2;
+            };
+
+
+
+            Expression<Func<int, int, double>> myLambda;
+
+            myLambda = (x, y) => x / (double)y;
+
+
+            //myLambda.
+
+
+
+
+            IQueryable<person> allPeopleInTheDatabase = GetAllPeopleSOmehow(); // SELECT * FROM person_table
+
+            IQueryable<person> peopleWhosLastNameStartsWithA = allPeopleInTheDatabase.Where(p => p.last_name.StartsWith("A")); // SELECT * FROM person_table WHERE last_name LIKE 'A%'
+
+            IQueryable<person> gimmeJust100Records = peopleWhosLastNameStartsWithA.Take(100); // SELECT TOP 100 * FROM person_table WHERE last_name LIKE 'A%'
+
+            //var people = GetAllPeopleSOmehow().ToList().Where(p => p.last_name.StartsWith("A")).Take(100);
+
+            var mySTuff = peopleWhosLastNameStartsWithA.Select(p => new
+            {
+                FirstName = p.first_name,
+                LastName = p.last_name
+            });
+
+
+
+            List<person> peopleInMemory = gimmeJust100Records.ToList();
+
+            foreach (var person in gimmeJust100Records)
+            {
+
+
+            }
+
+
+
+
+
+        }
+
+        private IQueryable<person> GetAllPeopleSOmehow()
+        {
+            throw new NotImplementedException();
+        }
+
+        int Add(int a, int b)
+        {
+            return a + b;
+
+        }
+
+        double Divide(int a, int b)
+        {
+            return a / (double)b;
+        }
+
+
+
     }
+
+
+
 }
