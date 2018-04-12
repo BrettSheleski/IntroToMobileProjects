93b3f9d733220e22d08e359b84d061f87a4378db
Brett Sheleski <brett@sheleski.com>
2018-04-12 12:48:44 -0500

    Added another demo app

---
+++ Demo Apps/Supplementary/EmbeddedResourcesExample/EmbeddedResourcesExample/EmbeddedResourcesExample/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EmbeddedResourcesExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            string theJson = "{}";
+
+
+            var assembly = typeof(MainPage).Assembly;
+
+
+            const string myDataJsonResourceName = "EmbeddedResourcesExample.mydata.json";
+
+
+            using (var resourceStream = assembly.GetManifestResourceStream(myDataJsonResourceName))
+                using (var reader = new System.IO.StreamReader(resourceStream))
+            {
+
+                theJson = reader.ReadToEnd();
+            }
+
+
+
+            List<person> people = Newtonsoft.Json.JsonConvert.DeserializeObject<List<person>>(theJson);
+
+
+
+            MyList.ItemsSource = people;
+        }
+    }
+}
---
+++ Demo Apps/Supplementary/EmbeddedResourcesExample/EmbeddedResourcesExample/EmbeddedResourcesExample/person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmbeddedResourcesExample
+{
+    public class person
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string country { get; set; }
+        public string email { get; set; }
+        public bool vip { get; set; }
+        public DateTime modified { get; set; }
+    }
+}
