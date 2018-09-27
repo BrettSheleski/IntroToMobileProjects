95210ec06c17e1e049b17966fefdeba45f38b487
Student <student@studentsmac135.fvtc.edu>
2018-09-26 19:11:04 -0500

    added week 5 example app

---
+++ Demo Apps/Week 5/CountdownTimer/CountdownTimer/CountdownTimerPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace CountdownTimer
+{
+    public partial class CountdownTimerPage : ContentPage
+    {
+        public CountdownTimerPage()
+        {
+            InitializeComponent();
+        }
+
+        TimeSpan updateInterval = TimeSpan.FromMilliseconds(100);
+
+        DateTime deathTime;
+
+        bool UpdateTimeLeftToLive()
+        {
+
+            TimeSpan timeLeftToLive = deathTime - DateTime.Now;
+
+            double secondsUntilDeath = timeLeftToLive.TotalSeconds;
+
+            RemainingTimeToLiveLabel.Text = secondsUntilDeath.ToString();
+
+            bool amIAlive;
+
+            if (DateTime.Now > deathTime)
+            {
+                amIAlive = false;
+
+                Device.BeginInvokeOnMainThread(DisplayYouAreDeadMessage);
+
+                this.UpdateChildrenLayout();
+            }
+            else
+            {
+                amIAlive = true;
+            }
+
+            return amIAlive;
+        }
+
+        void DisplayYouAreDeadMessage(){
+
+            YoureDeadLabel.IsVisible = true;
+            RemainingTimeToLiveLabel.Text = "0";
+
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            double secondsToLive;
+
+            if (double.TryParse(SecondsToLiveEntry.Text, out secondsToLive))
+            {
+
+                deathTime = DateTime.Now.AddSeconds(secondsToLive);
+
+                Device.StartTimer(updateInterval, UpdateTimeLeftToLive);
+
+            }
+        }
+    }
+}
