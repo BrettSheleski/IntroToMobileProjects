8e20cdf6f0b49bef3cb128bc8ea4368e2b7710eb
Student <student@students-mac-74.fvtc.edu>
2018-10-17 19:20:47 -0500

    added week7 temp converter

---
+++ Demo Apps/Week 7/TheGreatTemperatureConverter/TheGreatTemperatureConverter/TemperatureConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace TheGreatTemperatureConverter
+{
+    public class TemperatureConverter
+    {
+
+        public string Title { get; set; }
+
+        private double _input, _output;
+
+        public double Input
+        {
+            get
+            {
+                return _input;
+            }
+            set
+            {
+                _input = value;
+                _output = ConverterDelegate(_input);
+            }
+        }
+
+        public double Output { get { return _output; } }
+            
+
+        public Func<double, double> ConverterDelegate { get; set; }
+
+    }
+}
---
+++ Demo Apps/Week 7/TheGreatTemperatureConverter/TheGreatTemperatureConverter/TheGreatTemperatureConverterPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+
+namespace TheGreatTemperatureConverter
+{
+    public partial class TheGreatTemperatureConverterPage : ContentPage
+    {
+
+        CarouselView theCarousel;
+
+        public TheGreatTemperatureConverterPage()
+        {
+            InitializeComponent();
+
+
+            TemperatureConverter celsiusToF = new TemperatureConverter
+            {
+                Title = "Celsius To Fahrenheit??",
+                ConverterDelegate = c => (c * (9/5.0)) + 32
+            };
+
+            TemperatureConverter fToCelsius = new TemperatureConverter
+            {
+                Title = "Fahrenheight to Celsius",
+                ConverterDelegate = f => (f - 32) * (5 / 9.0)
+            };
+
+            TemperatureConverter[] converters = new TemperatureConverter[]{celsiusToF, fToCelsius };
+
+            TheCarousel.ItemsSource = converters;
+        }
+
+
+
+    }
+}
