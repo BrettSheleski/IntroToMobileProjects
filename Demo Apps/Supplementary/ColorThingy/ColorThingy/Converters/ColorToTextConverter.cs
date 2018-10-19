8b7f6b4aad1b1776f544a4253f1dccad28bab16a
Brett Sheleski <brett@sheleski.com>
2018-10-19 07:28:56 -0500

    added ColorThingy demo app

---
+++ Demo Apps/Supplementary/ColorThingy/ColorThingy/Converters/ColorToTextConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ColorThingy.Converters
+{
+    public class ColorToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color color)
+            {
+                // this sucks :(
+                // also has a performance problem... ;(
+                //return "Red: " + NumberToHex(color.R) + ", Blue: " + NumberToHex(color.G) + ", Green:" + NumberToHex(color.B);
+
+                // this sucks less...
+                // but performs better
+                //return string.Format("Red: {0}, Blue: {1}, Green: {2}", color.R, color.B, color.G);
+
+                
+
+                string r = NumberToHex(color.R * 255);
+                string g = NumberToHex(color.G * 255);
+                string b = NumberToHex(color.B * 255);
+
+                return $"{r}{g}{b}";
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue)
+            {
+                return Color.FromHex(stringValue);
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        string NumberToHex(double number)
+        {
+            return IntToHex((int)number);
+        }
+
+        string IntToHex(int number)
+        {
+            string hex = ("00" + number.ToString("X"));
+
+            hex = hex.Substring(hex.Length - 2);
+
+            return hex;
+        }
+    }
+}
---
+++ Demo Apps/Supplementary/ColorThingy/ColorThingy/Models/ColorPickerViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ColorThingy.Models
+{
+    public class ColorPickerViewModel : ViewModelBase
+    {
+        public ColorPickerViewModel()
+        {
+            InvertColorCommand = new Command(InvertColor);
+        }
+
+        public ICommand InvertColorCommand { get; }
+
+        private ushort _red, _green, _blue;
+
+        public Color Color
+        {
+            get
+            {
+                return Color.FromRgb(Red, Green, Blue);
+            }
+        }
+
+        public ushort Red
+        {
+            get { return _red; }
+            set
+            {
+                _red = value;
+                OnPropertyChanged();
+                OnPropertyChanged("Color");
+            }
+        }
+        public ushort Green
+        {
+            get { return _green; }
+            set
+            {
+                _green = value;
+                OnPropertyChanged();
+                OnPropertyChanged("Color");
+            }
+        }
+        public ushort Blue
+        {
+            get { return _blue; }
+            set
+            {
+                _blue = value;
+                OnPropertyChanged();
+                OnPropertyChanged("Color");
+            }
+        }
+
+        private void InvertColor()
+        {
+            ushort r, g, b;
+
+            r = Green;
+            g = Blue;
+            b = Red;
+
+            Red = r;
+            Green = g;
+            Blue = b;
+        }
+
+
+    }
+
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
