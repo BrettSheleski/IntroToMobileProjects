fb597213a111756778194fa8f1ff2b42bc04cb2d
Brett Sheleski <brett@sheleski.com>
2018-10-24 08:12:28 -0500

    Saving/loading to/from settings for FamilyList

--- Demo Apps/Week 8/FamilyList/FamilyList/FamilyList/App.xaml.cs
+++ Demo Apps/Week 8/FamilyList/FamilyList/FamilyList/App.xaml.cs
@@ -20,6 +20,53 @@ namespace FamilyList
             MainPage = navPage;
 		}
 
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            LoadFamilyFromProperties();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            LoadFamilyFromProperties();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+
+            SaveFamilyToProperties();
+        }
+
+        const string FAMILY_MEMBERS_PROPERTIES_KEY = "FamilyMembers";
+
+        public static void LoadFamilyFromProperties()
+        {
+            FamilyMembers.Clear();
+
+            if (Application.Current.Properties.ContainsKey(FAMILY_MEMBERS_PROPERTIES_KEY))
+            {
+                string json = Application.Current.Properties[FAMILY_MEMBERS_PROPERTIES_KEY] as string;
+
+                if (json != null)
+                {
+                    List<FamilyMember> theFam = Newtonsoft.Json.JsonConvert.DeserializeObject<List<FamilyMember>>(json);
+
+                    FamilyMembers.AddRange(theFam);
+                }
+            }
+        }
+
+        public static void SaveFamilyToProperties()
+        {
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(FamilyMembers);
+
+            Application.Current.Properties[FAMILY_MEMBERS_PROPERTIES_KEY] = json;
+        }
+
         public static List<FamilyMember> FamilyMembers { get; } = new List<FamilyMember>();
 	}
 }
