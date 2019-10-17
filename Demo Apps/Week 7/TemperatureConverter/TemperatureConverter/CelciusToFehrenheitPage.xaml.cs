ce6b9b7920a28995db6dab31df6a416fb387242e
Student <student@Students-Mac-121.local>
2019-10-16 19:22:55 -0500

    added temperature converter

---
+++ Demo Apps/Week 7/TemperatureConverter/TemperatureConverter/CelciusToFehrenheitPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TemperatureConverter
+{
+    public partial class CelciusToFehrenheitPage : ContentPage
+    {
+        public CelciusToFehrenheitPage()
+        {
+            InitializeComponent();
+        }
+
+
+        const double nineFifths = 9 / 5.0;
+
+        void HandleCalculateButton_Click(object sender, EventArgs e)
+        {
+            string degreesCString = CelciusEntry.Text;
+
+            double degreesC;
+
+            if (double.TryParse(degreesCString, out degreesC))
+            {
+                double degreesF = 32 + (nineFifths * degreesC);
+
+                FahrenheitEntry.Text = degreesF.ToString();
+            }
+        }
+    }
+}
---
+++ Demo Apps/Week 7/TemperatureConverter/TemperatureConverter/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TemperatureConverter
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void HandlC2F_Clicked(object sender, EventArgs e)
+        {
+            CelciusToFehrenheitPage page = new CelciusToFehrenheitPage();
+
+            Navigation.PushAsync(page);
+        }
+
+        void HandlF2C_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
