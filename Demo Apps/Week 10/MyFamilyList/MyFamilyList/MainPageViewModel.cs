2101d90adb25e2d25b88aad7349ebaf0080b67e8
Student <student@Students-Mac-84.local>
2019-11-06 18:41:12 -0600

    added MyFamilyList

---
+++ Demo Apps/Week 10/MyFamilyList/MyFamilyList/MainPageViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyFamilyList
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            AddCommand = new Command(Add);
+
+            FamilyMembers.Add(new Person
+            {
+                FirstName = "Brett",
+                LastName = "Awesomeski",
+                BirthDate = new DateTime(1984, 2, 12)
+            });
+
+
+            FamilyMembers.Add(new Person
+            {
+                FirstName = "Sarah",
+                LastName = "Sheleski",
+                BirthDate = new DateTime(1980, 5, 6)
+            });
+
+            FamilyMembers.Add(new Person
+            {
+                FirstName = "Steven",
+                LastName = "Sheleski",
+                BirthDate = new DateTime(2016, 2, 14)
+            });
+
+            FamilyMembers.Add(new Person
+            {
+                FirstName = "Gaby",
+                LastName = "Dilley",
+                BirthDate = new DateTime(2008, 1, 11)
+            });
+        }
+
+
+        public List<Person> FamilyMembers { get; } = new List<Person>();
+
+        public Command AddCommand { get; } 
+
+        public Xamarin.Forms.INavigation Navigation { get; set; }
+
+        void Add()
+        {
+            AddFamilyMemberPage page = new AddFamilyMemberPage();
+
+            Navigation.PushAsync(page);
+
+
+            // do something here!
+        }
+
+    }
+}
---
+++ Demo Apps/Week 10/MyFamilyList/MyFamilyList/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MyFamilyList
+{
+    public class Person
+    {
+
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string DisplayName
+        {
+            get
+            {
+                //return FirstName + " " + LastName;
+                //return string.Format("{0} {1}", FirstName, LastName);
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
