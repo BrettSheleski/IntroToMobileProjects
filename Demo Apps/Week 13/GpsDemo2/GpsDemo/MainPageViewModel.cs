74a47e0da5c0ae32ec1068708189d5643bddb40f
Student <student@Students-Mac-84.local>
2019-11-27 18:24:10 -0600

    added new gps demo

---
+++ Demo Apps/Week 13/GpsDemo2/GpsDemo/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace GpsDemo
+{
+    public class MainPageViewModel : ViewModel
+    {
+        private double latitude;
+        private double longitude;
+
+        public MainPageViewModel()
+        {
+            this.UpdateCommand = new Command(Update);
+        }
+
+        public double Latitude
+        {
+            get
+            {
+                return latitude;
+            }
+            set
+            {
+                latitude = value;
+                OnPropertyChanged();
+            }
+        }
+        public double Longitude { get => longitude; set => Set(ref longitude, value); }
+
+        public Command UpdateCommand { get; }
+
+        async void Update()
+        {
+
+            Location location = await Geolocation.GetLastKnownLocationAsync();
+
+            this.Latitude = location?.Latitude ?? 0;
+            this.Longitude = location?.Longitude ?? 0;
+
+        }
+
+    }
+}
